4edf042aff1ed44b191505653bc0e134349ab4f0
sabbir24680 <sabbirhossai928@gmail.com>
2024-10-06 02:08:32 +0600

    config_tmp.js

--- src/config/config_tmp.js
+++ src/config/config_tmp.js
@@ -1,4 +1,4 @@
 export class Config {
-  static TELEGRAM_APP_ID = undefined; // YOUR APP ID EX : 324893724923
-  static TELEGRAM_APP_HASH = undefined; // YOUR APP HASH EX: "aslkfjkalsjflkasf"
+  static TELEGRAM_APP_ID = 24588335; // YOUR APP ID EX : 324893724923
+  static TELEGRAM_APP_HASH = "063bda2718dd2feb99262420d9392d05"; // YOUR APP HASH EX: "aslkfjkalsjflkasf"
 }
